520c54805d0465540326014031fdb877d8b673c2
JVR-07 <machado.jvr07@gmail.com>
2025-04-19 20:13:59 -0700

    Creando proyecto en consola con C#

---
+++ chash-overflow-source/Illustrator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chash_overflow_source
+{
+    internal static class Illustrator
+    {
+
+        public static void PrintMenuSection(float CurrentBalance)
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("|\n|        DASHBORD GENERAL");
+            Console.Write("|\n|        Saldo Actual: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"${CurrentBalance}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("|\n|        OPCIONES A REALIZAR");
+            Console.WriteLine("|\n|    1.- Ingresar Saldo Actual");
+            Console.WriteLine("|\n|    e.- Salir del programa");
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("\n\nIngrese el caracter de la opción deseada: ");
+        }
+
+        public static float PrintAddSection()
+        {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("|\n|        CAPTURANDO SALDO ACTUAL");
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("|\n|    Ingrese el saldo actual: ");
+            float CurrentBalance = Convert.ToSingle(Console.ReadLine());
+            return CurrentBalance;
+        }
+    }
+}
---
+++ chash-overflow-source/Program.cs
@@ -0,0 +1,33 @@
+﻿using chash_overflow_source;
+
+//*******************************************************************
+// Variables
+bool Running = false;
+char SelectedOption = ' ';
+float CurrentBalance = 0f;
+//*******************************************************************
+do
+{
+    Console.Clear();
+    Running = true;
+    Illustrator.PrintMenuSection(CurrentBalance);
+    SelectedOption = Convert.ToChar(Console.ReadLine());
+
+    switch (SelectedOption)
+    {
+        case '1': 
+            {
+                Console.Clear();
+                CurrentBalance = Illustrator.PrintAddSection();
+            }
+            break;
+        case 'e': 
+            {
+                Running = false; 
+            } 
+            break;
+        default: { } break;
+    }
+}
+while (Running == true);
+
